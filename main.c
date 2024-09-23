c230f5152172c5dbfd991e4cdbe1b76c419b7811
Ubuntu <ubuntu@ip-172-31-86-139.ec2.internal>
2024-09-23 11:15:25 +0000

    DevB

--- main.c
+++ main.c
@@ -3,8 +3,8 @@ def add_numbers(num1, num2):
     return num1 + num2
 
 # Example usage
-number1 = 5
-number2 = 3
+number4 = 5
+number5 = 3
 
 result = add_numbers(number1, number2)
 print(f"The sum of {number1} and {number2} is: {result}")
